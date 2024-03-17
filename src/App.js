9bb7b94e9d0a3f817e0e70592de4d1f2f830f5cf
Bablu <singhbablukumar279@gmail.com>
2024-03-17 22:59:30 +0530

    Updated features

--- src/App.js
+++ src/App.js
@@ -1,19 +1,85 @@
 
+import { useState, useRef } from 'react';
 import './App.css';
-import React, { Component } from 'react'
-import NavBar from './Components/NavBar';
-import News from './Components/News';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-      <NavBar/>
-      <News/>
-      </div>
-      
-    )
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  const [isEditing, setIsEditing] = useState(false); // State to track editing status
+  const editId = useRef(null); // Ref to store the ID of the todo being edited
+// console.log(isEditing,editId.current)
+  const addTodo = () => {
+    
+    if (input.trim()) {
+      const newTodo = { id: Date.now(), text: input };
+      setTodos([...todos, newTodo]);
+      setInput('');
+    } else {
+      alert('Please Enter Valid Todo....');
+    }
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const updateTodo=(updatedtext)=>{
+    if(updatedtext.trim()){
+      setTodos(
+
+        todos.map((todo)=>todo.id=== editId.current? {...todo,text:updatedtext} : todo )
+      );
+      setIsEditing(false);
+      editId.current=null;
+      setInput("")
+    }else{
+      alert("update with some text..")
+    }
   }
+ 
+
+  return (
+    <>
+      <div className="container">
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          value={input}
+          name="username"
+          placeholder="some text here.."
+        />
+        <button onClick={addTodo}>Submit</button>
+      </div>
+      <div className="todolist">
+        <ul>
+          {todos.map((item) => (
+            <li key={item.id}>
+              {
+                isEditing && editId.current=== item.id ?
+                 (
+                  <input type='text' defaultValue={item.text} onBlur={(e)=>updateTodo(e.target.value)}/>
+                ):(
+                 <>
+                  {item.text}
+                  <i className="fa fa-edit" onClick={() => {
+                    setIsEditing(true);
+                    editId.current=item.id;
+                    }}>
+
+                  </i>
+                 </>
+                  
+                )
+
+              }
+             
+              <i className="fa fa-trash" onClick={() => deleteTodo(item.id)}></i>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
-
+export default App;
